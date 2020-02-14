d6867e80e07913ab743f3601cb5ec04b12cd6802
Diego Campanella <diegocampanella97@gmail.com>
2020-02-14 21:14:17 +0100

    start project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+// require('./jquery-migrate.min');
+require('./jquery.easing');
+require('./jquery.waypoints.min');
+require('./jquery.stellar.min');
+require('./owl.carousel.min');
+require('./jquery.magnific-popup.min');
+require('./aos');
+require('./jquery.animateNumber.min');
+require('./bootstrap-datepicker');
+require('./jquery.timepicker.min');
+require('./scrollax.min');
+require('./app');
