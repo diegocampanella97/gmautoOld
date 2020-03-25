596ebcd6a1d68f0414199b5d7b64eef39285e36a
Diego Campanella <diegocampanella97@gmail.com>
2020-03-25 23:15:28 +0100

    sistemate frasi di ringraziamento

--- resources/views/noleggio/thanks.blade.php
+++ resources/views/noleggio/thanks.blade.php
@@ -4,24 +4,27 @@
 <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('/images/bg_3.jpg');" data-stellar-background-ratio="0.5">
   <div class="overlay"></div>
   <div class="container">
-    <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-      <div class="col-md-9 ftco-animate pb-5">
-          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Noleggio <i class="ion-ios-arrow-forward"></i></span></p>
-        <h1 class="mb-3 bread">Contatti</h1>
-      </div>
+    <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start">
+        <div class="col-12 text-center">
+            <h2 class="py-3 text-white">Grazie</h2>
+            <p class="lead">La tua richiesta è stata presa in carico, verrai ricontattato il prima possibile da un nostro specialista.</p>
+        </div>
     </div>
   </div>
 </section>
+
 <section>
     <div class="container">
-        <div class="row py-4">
-            <div class="col-12 text-center">
-                <h3>Grazie</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laudantium unde id, dolore sint molestiae inventore! Quod, quisquam doloribus? Maiores amet cumque suscipit quis, natus assumenda saepe? Dolorem, officiis facilis?
-                  
-                </p>
+        <div class="row text-center py-5">
+            <div class="col-12">
+                <a href="{{route('home')}}">
+                    <button class="mx-3 my-3 btn btn-primary rounded">
+                        <h4 class="text-white"> Torna Indietro </h4>
+                    </button>
+                </a>
             </div>
         </div>
     </div>
+
 </section>
 @endsection
