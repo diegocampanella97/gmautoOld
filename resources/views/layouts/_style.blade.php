4ce9b34fd7af5a837ea99d54968a92581b742a9f
Diego Campanella <diegocampanella97@gmail.com>
2020-05-21 10:17:40 +0200

    merge css unique file

--- resources/views/layouts/_style.blade.php
+++ resources/views/layouts/_style.blade.php
@@ -1,26 +1,7 @@
 <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
 
-<link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css">
-<link rel="stylesheet" href="/css/animate.css">
-
-<link rel="stylesheet" href="/css/owl.carousel.min.css">
-<link rel="stylesheet" href="/css/owl.theme.default.min.css">
-<link rel="stylesheet" href="/css/magnific-popup.css">
-
-<link rel="stylesheet" href="/css/aos.css">
-
-<link rel="stylesheet" href="/css/ionicons.min.css">
-
-<link rel="stylesheet" href="/css/bootstrap-datepicker.css">
-<link rel="stylesheet" href="/css/jquery.timepicker.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css">
-
-
-<link rel="stylesheet" href="/css/flaticon.css">
-<link rel="stylesheet" href="/css/icomoon.css">
-<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="/css/full.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 @livewireStyles
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
