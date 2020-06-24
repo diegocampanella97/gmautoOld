8e28c1272518012a224bcb3e11a368ec8d8ae6c8
Diego Campanella <diegocampanella97@gmail.com>
2020-06-24 09:19:34 +0200

    add tag google analytics

--- resources/views/layouts/_style.blade.php
+++ resources/views/layouts/_style.blade.php
@@ -7,3 +7,12 @@
 <livewire:styles>
 @livewireStyles
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-170619759-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-170619759-1');
+</script>
