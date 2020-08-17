08c2336d16f3cc6aeb18b83aea6dd52b69c3571f
Diego Campanella <diegocampanella97@gmail.com>
2020-08-17 15:04:01 +0200

    send post facebook page complete

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,39 +9,34 @@ class TestController extends Controller
 {
     public function index($id){
 
-        $car = Car::find($id);
+        $car = Car::with('images')->find($id);
 
-        //dd($car->images()->get()->first()->filePath);
-
-        //dd($car->images()->first()->getUrl(800,570));
         try {
             $fb = new Facebook([
-                'app_id' => '1007560723018444',
-                'app_secret' => 'd300ec6125306b39afff5603fe5da99d',
+                'app_id' => getenv('FACEBOOK_APP_ID'),
+                'app_secret' => getenv('FACEBOOK_APP_SECRET'),
             ]);
         } catch (FacebookSDKException $e) {
             echo $e;
         }
 
-        $header = "Nuovi Arrivi!!! \n\n\nhttps://www.gmautoveicoli.it";
+        $header = "Nuovi Arrivi!!! \n\n\n";
+
+        $body = $car->name."\n\nScopri l’offerta sul sito: "."\n".route('auto.dettaglio',['id' => $car->id, 'slug' => $car->slug ]);
 
         $footer = "La vettura è a disposizione per la visione e qualsiasi controllo in ogni momento tramite appuntamento preventivo presso la nostra sede.\n\n\nNON ESITATE A CONTATTARCI PER QUALSIASI INFORMAZIONE, SIAMO A VOSTRA COMPLETA DISPOSIZIONE!
         \nACCETTIAMO LE VOSTRE VETTURE IN PERMUTA ALLE MIGLIORI QUOTAZIONI DI MERCATO \nPotete contattarci su whatsapp mandandoci foto e informazioni della vostra vettura per avere una quotazione indicativa.\n\n\nTelefono/Whatsapp: Gianluca : 3389807371";
 
 //        incorporamento header body footer
-        $message = $header."\n\n".$car->name."\n\nScopri l’offerta sul sito: "."\n".route('auto.dettaglio',['id' => $car->id, 'slug' => $car->slug ])."\n\n".$footer;
+        $messagePost = $header."\n\n".$body."\n\n".$footer;
 
         $linkData = [
-            'message' => $message,
+            'message' => $messagePost,
             'url' => "gmautoveicoli.it".$car->images()->first()->getUrl(800,570)
         ];
 
-
-
-        $pageAccessToken ='EAAOUXwL8MswBALcELMHgYhfztvqabrETpJMP2jfh1qJKU0y0y65F540MWD07fiuZCLaQewFcvfjluvGnT5Om3ZA5R4GiJnvFVeZADBwpeiAQZAmt6kw1LO2oQhrZCgh0THNNDTc1RQ1sHQg74mZAAIucb5X9gKuh5ZB73oZCcFGDRKzTHD79E53A6dhcvjZA2ZBM5KnqQ5ZBHaHZBZBMZBAUbDgZCKC';
-
         try {
-            $response = $fb->post('2000966496847873/photos', $linkData, $pageAccessToken);
+            $response = $fb->post(getenv('FACEBOOK_PAGE_ID').'/photos', $linkData, getenv('FACEBOOK_ACCESS_TOKEN'));
         } catch(Facebook\Exceptions\FacebookResponseException $e) {
             echo 'Graph returned an error: '.$e->getMessage();
             exit;
